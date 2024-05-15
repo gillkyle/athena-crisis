e19de18baf3d447ba693df131bbc759093031949
Kyle Gill <kylerobertgill@gmail.com>
2024-05-14 21:19:14 -0600

    hacky tweaks to test changes (cherry pick these out)

--- docs/content/playground/PlaygroundGame.tsx
+++ docs/content/playground/PlaygroundGame.tsx
@@ -1,9 +1,13 @@
 import { MapMetadata } from '@deities/apollo/MapMetadata.tsx';
 import { prepareSprites } from '@deities/art/Sprites.tsx';
+import { Sniper } from '@deities/athena/info/Unit.tsx';
 import MapData from '@deities/athena/MapData.tsx';
 import GameMap from '@deities/hera/GameMap.tsx';
 import useClientGame from '@deities/hera/hooks/useClientGame.tsx';
 import useClientGameAction from '@deities/hera/hooks/useClientGameAction.tsx';
+import { UserLike } from '@deities/hera/hooks/useUserMap.tsx';
+import { hasNotableAnimation } from '@deities/hera/MapAnimations.tsx';
+import CurrentGameCard from '@deities/hera/ui/CurrentGameCard.tsx';
 import GameActions from '@deities/hera/ui/GameActions.tsx';
 import MapInfo from '@deities/hera/ui/MapInfo.tsx';
 import useScale from '@deities/ui/hooks/useScale.tsx';
@@ -36,6 +40,19 @@ export default function PlaygroundGame({
   const ref = useRef(null);
   const isInView = useInView(ref, { margin: '-20% 0px 6% 0px' });
 
+  const viewer = {
+    access: 'User',
+    character: {
+      unitId: Sniper.id,
+      variant: 0,
+    },
+    displayName: 'Hot reloads!',
+    factionName: 'Atlas',
+    id: 'Demo-User-12',
+    skills: [1],
+    username: 'demo-maxima',
+  } as UserLike;
+
   return (
     <div ref={ref}>
       <GameMap
@@ -52,7 +69,7 @@ export default function PlaygroundGame({
         scale={zoom}
         scroll={false}
         style="floating"
-        tilted
+        tilted={true}
         userDisplayName="Player"
       >
         {(props, actions) => {
@@ -61,6 +78,26 @@ export default function PlaygroundGame({
 
           return (
             <>
+              <CurrentGameCard
+                actions={actions}
+                animatePlayer={hasNotableAnimation(props.animations)}
+                currentViewer={props.currentViewer}
+                hide={hide}
+                inlineUI={props.inlineUI}
+                inset={0}
+                map={props.map}
+                teams={[...props.map.teams.sortBy(({ id }) => id).values()].map(
+                  (team) => ({
+                    team,
+                    users: new Map([
+                      [1, viewer],
+                      [2, viewer],
+                    ]),
+                  }),
+                )}
+                vision={props.vision}
+                zIndex={props.zIndex}
+              />
               <MapInfo hide={hide} leftOffset {...props} />
               <GameActions
                 actions={actions}
